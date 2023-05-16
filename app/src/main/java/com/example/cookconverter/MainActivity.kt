41e7efbe0af491a5d421e81c6e5abecf5a20c1cd
natakozak <kozaknata02@gmail.com>
2023-05-16 22:47:21 +0300

    first

---
+++ app/src/main/java/com/example/cookconverter/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.cookconverter
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+
+
+}
